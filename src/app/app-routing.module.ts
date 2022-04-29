cfe6568dde0a5ca67e815d3db837d44e5004b31f
Maria Gabriela <mgvillarreal09@gmail.com>
2022-04-29 19:41:17 -0300

    ingreso rooting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IngresoComponent } from './page/ingreso/ingreso.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ingreso', component: IngresoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
