bc0db9b18bbd6c6c9ae09502ff5786fec22d9e57
Jorge Calderón <Jorgedcalderon1989@gmail.com>
2022-05-13 18:46:55 -0300

    MovimientoService

--- src/app/services/movimientos.service.ts
+++ src/app/services/movimientos.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Movimiento } from '../models/movimiento.model';
 
@@ -8,8 +9,22 @@ export class MovimientosService {
 
   movimiento: Movimiento = new Movimiento
 
-  movimientos: [] = []
+  url = 'https://hostinjor.com/finperapi/api/movimientos/'
 
-  constructor() { }
+  constructor(
+    public http: HttpClient
+  ) { }
 
+
+  traeMovimientos(){
+    return this.http.get<any>(this.url);
+  }
+
+  traeUnMovimiento(id: string){
+    return this.http.get<Movimiento>(id);
+  }
 }
+
+
+
+
