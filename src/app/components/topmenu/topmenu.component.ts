588d769bb3548681a4393c5a79fa71f412a06282
Maria Gabriela <mgvillarreal09@gmail.com>
2022-06-17 19:55:43 -0300

    cambio despliegue del menu

--- src/app/components/topmenu/topmenu.component.ts
+++ src/app/components/topmenu/topmenu.component.ts
@@ -7,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopmenuComponent implements OnInit {
 
+  menu:string = "cerrado";
+
   constructor() { }
+
+  cambiaEstadoMenu(): void{
+    if(this.menu === 'abierto')
+    {
+      this.menu = 'cerrado';
+    }
+    else
+    {
+      this.menu = 'abierto';
+    }
+    
+  }
 
   ngOnInit(): void {
   }
