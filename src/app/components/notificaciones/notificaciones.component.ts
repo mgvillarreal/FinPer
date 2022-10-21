a16354ed25ca2609bd32dd2aee62973242038e3e
Maria Gabriela <mgvillarreal09@gmail.com>
2022-10-21 20:10:21 -0300

    agrego mensajes notificaciones

--- src/app/components/notificaciones/notificaciones.component.ts
+++ src/app/components/notificaciones/notificaciones.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificacionesComponent implements OnInit {
 
+  /* BANDERAS */
+  muestraTodasNotifFlag = 1;
+  muestraNotificacionFlag = 0;
+  preguntaEliminNotifFlag = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  volveraNotificaciones(){
+    this.muestraNotificacionFlag = 0;
+    this.muestraTodasNotifFlag = 1;
+  }
+
+  muestraNotificacion(){
+    if(this.muestraNotificacionFlag == 0){
+      this.muestraNotificacionFlag = 1;
+      this.muestraTodasNotifFlag = 0;
+    }
+  }
+
+  preguntaEliminarNotificacion(){
+    if(this.preguntaEliminNotifFlag == 0){
+      this.preguntaEliminNotifFlag = 1;
+      this.muestraTodasNotifFlag = 0;
+    }
+  }
+
+  eliminaNotificacion(){
+    this.volveraNotificaciones();
   }
 
 }
