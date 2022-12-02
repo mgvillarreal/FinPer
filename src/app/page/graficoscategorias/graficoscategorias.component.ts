1d8f2404799c1263ef58c071f24b75b0deae767d
Maria Gabriela <mgvillarreal09@gmail.com>
2022-12-01 21:49:30 -0300

    mejora informe mensual x categoria

--- src/app/page/graficoscategorias/graficoscategorias.component.ts
+++ src/app/page/graficoscategorias/graficoscategorias.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Chart, ChartConfiguration, ChartItem, registerables } from 'chart.js';
+import { Chart, ChartConfiguration, ChartItem, layouts, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-graficoscategorias',
@@ -8,11 +8,21 @@ import { Chart, ChartConfiguration, ChartItem, registerables } from 'chart.js';
 })
 export class GraficoscategoriasComponent implements OnInit {
 
+  arrMeses:string[] = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+  arrAnios:number[] = [2021, 2022];
+  mesActual:number = Number(new Date().getMonth());
+  anioActual:number =  Number(new Date().getFullYear());
+
   constructor() { }
 
   ngOnInit(): void {
     this.creaGraficoIngresos();
     this.creaGraficoGastos();
+  }
+
+  changeMes(){
+    this.mesActual = Number(this.mesActual);
+    this.anioActual = Number(this.anioActual);
   }
 
   creaGraficoIngresos(): void {
@@ -33,12 +43,38 @@ export class GraficoscategoriasComponent implements OnInit {
           '#57AB33'
         ],
         hoverOffset: 4
-      }]
+      }]    
     };
 
     const config: ChartConfiguration = {
       type: 'pie',
       data: data,
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: 'Ingresos por Categoría',
+            font: {
+              size: 18,
+              family: 'Poppins, sans-serif',
+            }
+          },
+          legend: {
+            position: 'right',
+            labels: {
+              font: {
+                size: 11,
+                family: 'Poppins, sans-serif',
+              }
+            }
+          }
+        },
+        layout: {
+          padding: {
+            left: 15
+          }
+        }
+      }
     };
 
     const chartItem: ChartItem = document.getElementById('grafico-ingresosmensual') as ChartItem;
@@ -82,6 +118,32 @@ export class GraficoscategoriasComponent implements OnInit {
     const config: ChartConfiguration = {
       type: 'pie',
       data: data,
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: 'Gastos por Categoría',
+            font: {
+              size: 18,
+              family: 'Poppins, sans-serif',
+            }
+          },
+          legend: {
+            position: 'right',
+            labels: {
+              font: {
+                size: 11,
+                family: 'Poppins, sans-serif',
+              }
+            }
+          }
+        },
+        layout: {
+          padding: {
+            left: 15
+          }
+        }
+      }
     };
 
     const chartItem: ChartItem = document.getElementById('grafico-gastosmensual') as ChartItem;
