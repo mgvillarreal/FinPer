85ec644b73572365b0aa3c5bcceb7f9ee30ec37a
Maria Gabriela <mgvillarreal09@gmail.com>
2023-02-23 00:35:46 -0300

    grafico mensual categorias trae datos de la BBDD

--- src/app/page/principal/principal.component.ts
+++ src/app/page/principal/principal.component.ts
@@ -19,7 +19,6 @@ import { MetasService } from 'src/app/services/metas.service';
 export class PrincipalComponent implements OnInit {
   ingreso: number;
   egreso: number;
-  //ahorro: number = 17800;
   balance: number = 0;
   monto: Number = 0;
   montoString: string;
