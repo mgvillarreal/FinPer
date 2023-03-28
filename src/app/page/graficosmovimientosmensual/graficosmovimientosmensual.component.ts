cf363a80d0b9e1725eaa74ef8020e32f4576295d
Maria Gabriela <mgvillarreal09@gmail.com>
2023-03-28 14:44:51 -0300

    hago responsive grafico mensual

--- src/app/page/graficosmovimientosmensual/graficosmovimientosmensual.component.ts
+++ src/app/page/graficosmovimientosmensual/graficosmovimientosmensual.component.ts
@@ -144,6 +144,8 @@ export class GraficosmovimientosmensualComponent implements OnInit {
       type: 'bar',
       data: data,
       options: {
+        responsive: true,
+        maintainAspectRatio: false,
         scales: {
           y: {
             beginAtZero: true
